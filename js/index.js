ca82aa6f5589f37a2e2692dad22459b986c28edd
elizabethzi <eb030104@gmail.com>
2023-06-04 14:28:36 -0500

    actualizacion 3

--- js/index.js
+++ js/index.js
@@ -1,25 +1,29 @@
-
+//Funcion validaciones
 function Validacion(){
     let tareas = document.getElementById('tareas').value;
 
+    //Variable que abarca solo letras
     var regex = /[^a-zA-Z\s]/;
 
+    //Si el input esta vacio, pide agregar caracteres
     if(tareas == ""){
         Swal.fire({ title: "Agrega una tarea",
         icon: 'warning',
         confirmButtonColor: '#c3f0ca', type: "success"});
         return false;
-    }else if(regex.test(tareas)){
+
+    }else if(regex.test(tareas)){ //Se realiza la comparacion de datos ingresados en el input con la variable regex gracias al metodo test para verificar si los caracteres son letras o no
         Swal.fire({ title: "No puedes agregar números o caracteres especiales",
         icon: 'error'}); 
         return false;
-    }else if(tareas.length <3){
+    }else if(tareas.length <3){ //Se verifica el número de caracteres, de ser menor a tres, arroja false
         Swal.fire({title:"La tarea es muy corta", text:"¡Agrega mas detalles!", icon:'info'});
         return false;
     }
     return true;
 }
 
+//Funcion
 function leerTarea(){
     let listaTarea;
     if(localStorage.getItem('listaTarea') == null){
@@ -39,6 +43,7 @@ document.querySelector('#lista').innerHTML = html;
 
 document.onload = leerTarea();
 
+//Funcion boton
 function Enviar(){
     if(Validacion() == true){
         let tareas = document.getElementById('tareas').value;
@@ -111,4 +116,28 @@ listaTarea = []
 
 }
 
+//Función evento para guardar datos del input con el Enter
+document.addEventListener("DOMContentLoaded", function() {
+    const input = document.getElementById("tareas");
+  
+    input.addEventListener("keydown", function(event) {
+      if (event.keyCode === 13) {
+        const inputValue = event.target.value.trim();
+        event.preventDefault();
+        if (inputValue.length > 0) {
+          introEventHandler(inputValue);
+          Enviar();
+        
+        } else{
+            Swal.fire({ title: "Agrega una tarea",
+            icon: 'warning',
+            confirmButtonColor: '#c3f0ca', type: "success"});
+            return false;
+        }
+      }
+    });
+  });
 
+  function introEventHandler(){};
+    
+  
